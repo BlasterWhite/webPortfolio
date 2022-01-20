05da8990144ca8198523b8fa4f3940658a09671d
maguezennec <mateo.guezennec@etudiant.univ-rennes1.fr>
2022-01-20 22:03:02 +0100

    Starting the home page

---
+++ js/calculdate.js
@@ -0,0 +1,16 @@
+let age = document.getElementById("age");
+
+
+
+
+
+function getAge(date) { 
+    var diff = Date.now() - date.getTime();
+    var age = new Date(diff); 
+    return `${(age / (1000 * 3600 * 24 * 365.25)).toFixed(7)} ans`;
+}
+
+setInterval(() => {
+    age.innerHTML = getAge(new Date("2003, 08, 18"));
+},1000);
+
