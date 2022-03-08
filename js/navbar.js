3ea9cd80c8711154afb4d945454159a8e8910136
maguezennec <mateo.guezennec@etudiant.univ-rennes1.fr>
2022-03-08 10:41:30 +0100

    Profile + Compétences + Contact

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,6 @@
-const btnMenu = ;
+const btnMenu = document.getElementById('btnBurger');
+const Nav = document.getElementsByTagName('nav');
+
+btnMenu.addEventListener('click', () => {
+    Nav[0].classList.toggle('responsiveNav');
+});
